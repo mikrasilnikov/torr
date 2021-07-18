84484f5fd16ff8f1da6244c9bc0c9cce4dcf08b5
mikrasilnikov <mi.krasilnikov@gmail.com>
2021-07-18 03:52:26 +0300

    *

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ lazy val root = (project in file("."))
       "dev.zio" %% "zio"          % "1.0.9",
       "dev.zio" %% "zio-nio-core" % "1.0.0-RC11",
       "dev.zio" %% "zio-nio"      % "1.0.0-RC11",
+      "io.d11"  %% "zhttp"        % "1.0.0.0-RC17",
       "dev.zio" %% "zio-test-sbt" % "1.0.9" % "test",
       "dev.zio" %% "zio-test"     % "1.0.9" % Test
     )
--- src/test/scala/torr/channels/test/BEncodeOverSocketsSuite.scala
+++ src/test/scala/torr/channels/test/BEncodeOverSocketsSuite.scala
@@ -62,7 +62,7 @@ object BEncodeOverSocketsSuite extends DefaultRunnableSpec {
           _           <- serverFiber.interrupt
         } yield assert(response)(equalTo("hello"))
 
-      },
+      } @@ timeout(5.seconds),
       //
       testM("Echo string") {
 
@@ -89,6 +89,44 @@ object BEncodeOverSocketsSuite extends DefaultRunnableSpec {
           response    <- createClient(client)
           _           <- serverFiber.interrupt
         } yield assert(response)(equalTo(("hello", "world")))
+
+      } @@ timeout(5.seconds),
+      //
+      testM("Sum of ints") {
+
+        def server(channel: AsyncSocketChannel): Task[Unit] = {
+
+          def performSum(channel: AsyncSocketChannel, buf: ByteBuffer) =
+            for {
+              list <- BEncode.read(channel, buf).map(_.asList.get)
+              res   = list.foldLeft(0L) { case (acc, bVal) => acc + bVal.asInstanceOf[BInt].value }
+              _    <- BEncode.write(BInt(res), channel, buf)
+            } yield ()
+
+          for {
+            buf <- Buffer.byte(1024)
+            _   <- performSum(channel, buf).repeatWhileM(_ => channel.isOpen)
+          } yield ()
+        }
+
+        def client(channel: AsyncSocketChannel): Task[(Long, Long, Long)] = {
+          import BValue._
+          for {
+            buf  <- Buffer.byte(1024)
+            _    <- BEncode.write(list(int(1), int(2), int(3)), channel, buf)
+            res1 <- BEncode.read(channel, buf).map(_.asInt.get)
+            _    <- BEncode.write(list(int(4), int(5), int(6)), channel, buf)
+            res2 <- BEncode.read(channel, buf).map(_.asInt.get)
+            _    <- BEncode.write(list(int(7), int(8), int(9)), channel, buf)
+            res3 <- BEncode.read(channel, buf).map(_.asInt.get)
+          } yield (res1, res2, res3)
+        }
+
+        for {
+          serverFiber <- createServer(server).fork
+          response    <- createClient(client)
+          _           <- serverFiber.interrupt
+        } yield assert(response)(equalTo((6L, 15L, 24L)))
 
       } @@ timeout(5.seconds)
     ) @@ sequential
