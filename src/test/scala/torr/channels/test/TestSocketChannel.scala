5407b889f09f387b2bc1d2a0939acc79d1b9abf2
mkrasilnikov <mi.krasilnikov@gmail.com>
2021-09-11 14:57:15 +0300

    peerwire wip

--- src/test/scala/torr/channels/test/TestSocketChannel.scala
+++ src/test/scala/torr/channels/test/TestSocketChannel.scala
@@ -5,69 +5,58 @@ import torr.channels.ByteChannel
 import zio.nio.core.ByteBuffer
 import scala.util.Random
 
-private case class DelayedReadRequest(buf: ByteBuffer, amount: Int, promise: Promise[Throwable, Unit])
+case class TestSocketChannel(incoming: Queue[Byte], outgoing: Queue[Byte], random: Random) extends ByteChannel {
 
-case class TestSocketChannel(semaphore: Semaphore, rnd: Random) extends ByteChannel {
+  def read(buf: ByteBuffer): Task[Int] = {
+    for {
+      remaining  <- buf.remaining
+      bytesToRead = random.between(1, remaining + 1)
+      chunk      <- incoming.takeN(bytesToRead).map(lst => Chunk.fromArray(lst.toArray))
+      _          <- buf.putChunk(chunk)
+    } yield bytesToRead
+  }
 
-  private var incoming: Chunk[Byte] = Chunk.empty
-  private var outgoing: Chunk[Byte] = Chunk.empty
-
-  private var readRequest: Option[DelayedReadRequest] = None
-
-  def read(buf: ByteBuffer): Task[Int] =
-    semaphore.withPermit {
-      for {
-        bufRemaining <- buf.remaining
-        bytesToRead   = rnd.between(1, bufRemaining + 1)
-
-        promise <- Promise.make[Throwable, Unit]
-        _       <- readOrDelayRequest(buf, bytesToRead, promise)
-        _       <- promise.await
-      } yield bytesToRead
-    }
-
-  def write(buf: ByteBuffer): Task[Int] =
-    semaphore.withPermit {
-      for {
-        bufRemaining <- buf.remaining
-        bytesToWrite  = rnd.between(1, bufRemaining + 1)
-
-        data <- buf.getChunk(bytesToWrite)
-        _     = outgoing = outgoing ++ data
-      } yield bytesToWrite
-    }
+  def write(buf: ByteBuffer): Task[Int] = {
+    for {
+      remaining   <- buf.remaining
+      bytesToWrite = random.between(1, remaining + 1)
+      chunk       <- buf.getChunk(bytesToWrite)
+      _           <- outgoing.offerAll(chunk)
+    } yield bytesToWrite
+  }
 
   def isOpen: Task[Boolean] = ZIO.succeed(true)
 
-  private def readOrDelayRequest(
-      buf: ByteBuffer,
-      remaining: Int,
-      promise: Promise[Throwable, Unit]
-  ): Task[Unit] = {
+  val remote = new ByteChannel {
+    def read(buf: ByteBuffer): Task[Int] = {
+      for {
+        remaining  <- buf.remaining
+        bytesToRead = random.between(1, remaining + 1)
+        chunk      <- outgoing.takeN(bytesToRead).map(lst => Chunk.fromArray(lst.toArray))
+        _          <- buf.putChunk(chunk)
+      } yield bytesToRead
 
-    (remaining, incoming) match {
-
-      case (0, _) => promise.succeed().as()
-
-      case (_, Chunk.empty) =>
-        readRequest = Some(DelayedReadRequest(buf, remaining, promise))
-        ZIO.unit
-
-      case _                =>
-        val bytesToCopy = math.min(remaining, incoming.length)
-        for {
-          _ <- buf.putChunk(incoming.take(bytesToCopy))
-          _  = incoming = incoming.drop(bytesToCopy)
-          _ <- readOrDelayRequest(buf, remaining - bytesToCopy, promise)
-        } yield ()
     }
+
+    def write(buf: ByteBuffer): Task[Int] = {
+      for {
+        remaining   <- buf.remaining
+        bytesToWrite = random.between(1, remaining + 1)
+        chunk       <- buf.getChunk(bytesToWrite)
+        _           <- incoming.offerAll(chunk)
+      } yield bytesToWrite
+    }
+
+    def isOpen: Task[Boolean] = ZIO.succeed(true)
   }
+
 }
 
 object TestSocketChannel {
   def make: Task[TestSocketChannel] =
     for {
-      s  <- Semaphore.make(1)
-      rnd = Random.javaRandomToRandom(new java.util.Random(42))
-    } yield TestSocketChannel(s, rnd)
+      incoming <- Queue.unbounded[Byte]
+      outgoing <- Queue.unbounded[Byte]
+      rnd       = Random.javaRandomToRandom(new java.util.Random(42))
+    } yield TestSocketChannel(incoming, outgoing, rnd)
 }
---
+++ src/test/scala/torr/channels/test/TestSocketChannelSpec.scala
@@ -0,0 +1,56 @@
+package torr.channels.test
+
+import zio._
+import zio.test._
+import zio.test.Assertion._
+import torr.channels.ByteChannel
+import zio.nio.core.Buffer
+import java.nio.charset.StandardCharsets
+
+object TestSocketChannelSpec extends DefaultRunnableSpec {
+  override def spec =
+    suite("TestSocketChannelSpec")(
+      //
+      testM("writing") {
+        val expected = Chunk.fromArray("Hello привет".getBytes(StandardCharsets.UTF_8))
+        for {
+          channel <- TestSocketChannel.make
+          _       <- writeAll(channel, expected)
+          actual  <- readAmount(channel.remote, expected.length)
+        } yield assert(actual)(equalTo(expected))
+      },
+      //
+      testM("reading") {
+        val expected = Chunk.fromArray("Hello привет".getBytes(StandardCharsets.UTF_8))
+        for {
+          channel <- TestSocketChannel.make
+          _       <- writeAll(channel.remote, expected)
+          actual  <- readAmount(channel, expected.length)
+        } yield assert(actual)(equalTo(expected))
+      }
+    )
+
+  //noinspection SimplifyUnlessInspection
+  def writeAll(channel: ByteChannel, data: Chunk[Byte]): Task[Unit] = {
+    if (data.isEmpty) ZIO.unit
+    else {
+      for {
+        buf     <- Buffer.byte(data)
+        written <- channel.write(buf)
+        _       <- writeAll(channel, data.drop(written))
+      } yield ()
+    }
+  }
+
+  def readAmount(channel: ByteChannel, amount: Int, acc: Chunk[Byte] = Chunk.empty): Task[Chunk[Byte]] = {
+    if (amount == 0) ZIO.succeed(acc)
+    else {
+      for {
+        buf       <- Buffer.byte(amount)
+        bytesRead <- channel.read(buf) <* buf.flip
+        chunk     <- buf.getChunk()
+        res       <- readAmount(channel, amount - bytesRead, acc ++ chunk)
+      } yield res
+    }
+  }
+}
